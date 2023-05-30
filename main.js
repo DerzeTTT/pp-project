8c0fbf0a452dec4aae6488efe63bcdc28504170d
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-05-30 11:40:55 +0200

    app foundation

---
+++ main.js
@@ -0,0 +1,37 @@
+// Electron app handler
+
+// Import modules
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+const path = require('path');
+
+// Window creation on ready
+app.on('ready', () => {
+
+    let mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+
+        webPreferences: {
+            nodeIntegration: true
+        },
+
+        autoHideMenuBar: true,
+
+        icon: './assets/app-assets/images/icon.png'
+
+    });
+
+    mainWindow.loadURL(`file://${path.join(__dirname, '/assets/web-page/index.html')}`);
+
+});
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+
+});
